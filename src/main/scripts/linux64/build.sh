8e1cbfe3ef99bba24a425c8eadd1032f04461385
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-09-12 07:11:36 +0200

    [YIL-324] Make the build fails if native compilation fails.

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -3,6 +3,7 @@ cp zlib/linux64/include/zutil.h ../../c++/Source/FreeImage
 cmake ../../c++ -DZLIB_INCLUDE_DIR="linux64/zlib/include" -DZLIB_LIBRARY="linux64/zlib/lib" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" -DENABLE_TIFF=0 -G "Unix Makefiles"
 
 make
+r1=$?
 
 rm -R CMakeFiles
 rm CMakeCache.txt
@@ -12,6 +13,8 @@ rm install_manifest.txt
 rm -R lib
 rm -R Source
 rm -R zlib
+
+return $r1
 
 
 
--- src/main/scripts/win32/build.sh
+++ src/main/scripts/win32/build.sh
@@ -1,9 +1,12 @@
 cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win32" -DENABLE_TIFF=0 -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 make
+r1=$?
 
 rm -R CMakeFiles
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
 rm install_manifest.txt
+
+return $r1

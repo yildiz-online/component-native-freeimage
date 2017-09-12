60c17a8070cdbcb364fdecd7216bb160c55c97fb
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-09-12 17:07:32 +0200

    [YIL-325] Freeimage native files.

--- src/main/scripts/win32/build.sh
+++ src/main/scripts/win32/build.sh
@@ -1,12 +1,15 @@
-cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win32" -DENABLE_TIFF=0 -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win32" -DZLIB_LIBRARY="zlib/win32/lib" -DZLIB_INCLUDE_DIR="zlib/win32/include"  -DENABLE_TIFF=0 -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 make
 r1=$?
+
+make clean
 
 rm -R CMakeFiles
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
 rm install_manifest.txt
+rm -R zlib
 
 return $r1

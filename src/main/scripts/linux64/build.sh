ad99d4e0e30f7acd39fe737aa76275ed534f39d6
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-09-20 16:59:40 +0200

    [YIL-325] Fix png inclusion.

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -1,6 +1,16 @@
 cp zlib/linux64/include/zutil.h ../../c++/Source/FreeImage
 
-cmake ../../c++ -DZLIB_INCLUDE_DIR="linux64/zlib/include" -DZLIB_LIBRARY="linux64/zlib/lib" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" -DENABLE_TIFF=0 -G "Unix Makefiles"
+cmake ../../c++ \
+-DZLIB_INCLUDE_DIRS="${PWD}/zlib/linux64/include" \
+-DZLIB_LIBRARY="${PWD}/zlib/linux64/lib/" \
+-DJPEG_INCLUDE_DIR="${PWD}/jpeg/linux64/include" \
+-DJPEG_LIBRARY="${PWD}/jpeg/linux64/lib/libjpeg.so" \
+-DPNG_PNG_INCLUDE_DIR="${PWD}/png/linux64/include" \
+-DPNG_INCLUDE_DIRS="${PWD}/png/linux64/include" \
+-DPNG_LIBRARY="${PWD}/png/linux64/lib/" \
+-DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" \
+-DENABLE_TIFF=0 \
+-G "Unix Makefiles"
 
 make
 r1=$?
@@ -13,6 +23,8 @@ rm install_manifest.txt
 rm -R lib
 rm -R Source
 rm -R zlib
+rm -R png
+rm -R jpeg
 
 return $r1
 
--- src/main/scripts/win32/build.sh
+++ src/main/scripts/win32/build.sh
@@ -1,4 +1,24 @@
-cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win32" -DZLIB_LIBRARY="zlib/win32/lib" -DZLIB_INCLUDE_DIR="zlib/win32/include"  -DENABLE_TIFF=0 -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+cp ${PWD}/png/win32/include/png.h ${PWD}/zlib/win32/include/
+cp ${PWD}/png/win32/include/pnglibconf.h ${PWD}/zlib/win32/include/
+cp ${PWD}/png/win32/include/pngconf.h ${PWD}/zlib/win32/include/
+cp ${PWD}/jpeg/win32/include/jinclude.h ${PWD}/zlib/win32/include/
+cp ${PWD}/jpeg/win32/include/jerror.h ${PWD}/zlib/win32/include/
+cp ${PWD}/jpeg/win32/include/jmorecfg.h ${PWD}/zlib/win32/include/
+cp ${PWD}/jpeg/win32/include/jpeglib.h ${PWD}/zlib/win32/include/
+cp ${PWD}/jpeg/win32/include/jconfig.h ${PWD}/zlib/win32/include/
+
+cmake ../../c++ \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX="../../../../target/classes/win32" \
+-DZLIB_LIBRARY="${PWD}/zlib/win32/lib/libzlibstatic.a" \
+-DZLIB_INCLUDE_DIR="${PWD}/zlib/win32/include" \
+-DJPEG_INCLUDE_DIR="${PWD}/jpeg/win32/include" \
+-DJPEG_LIBRARY="${PWD}/jpeg/win32/lib/libjpeg.dll.a" \
+-DPNG_PNG_INCLUDE_DIR="${PWD}/png/win32/include" \
+-DPNG_LIBRARY="${PWD}/png/win32/lib/libpng.a" \
+-DENABLE_TIFF=0 \
+-DENABLE_JPEG=0 \
+-DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 make
 r1=$?
@@ -11,5 +31,10 @@ rm cmake_install.cmake
 rm Makefile
 rm install_manifest.txt
 rm -R zlib
+rm -R jpeg
+rm -R png
+rm -R bin
+rm -R lib
+rm -R Source
 
 return $r1

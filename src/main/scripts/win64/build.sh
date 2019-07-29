4a2dc87e9d4add728e6345fb5a4897d8631a2865
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-07-29 16:48:55 +0200

    Update build.sh

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -9,7 +9,7 @@ export CPP=$PREFIX-cpp
 export RANLIB=$PREFIX-ranlib
 export PATH="/usr/$PREFIX/bin:$PATH"
 mingw-w64-x86_64-pkg-config --host=i686-w64-mingw32
-make
+make -f Makefile.mingw
 r1=$?
 mkdir -p ../../../../../target/classes/win64/include
 mkdir -p ../../../../../target/classes/win64/lib

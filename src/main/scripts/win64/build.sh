5de01fb583f202f5931e77b436b81d94e8b78afc
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-10-28 08:18:17 +0100

    [WIP] Compile windows binaries.

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
 
-chmod +x PKGBUILD
-./PKGBUILD
 cp -R ../../c++ workdir
 cd workdir
-PREFIX=i686-w64-mingw32
-export CC=$PREFIX-gcc
-export CXX=$PREFIX-g++
-export CPP=$PREFIX-cpp
-export RANLIB=$PREFIX-ranlib
-export PATH="/usr/$PREFIX/bin:$PATH"
-mingw-w64-x86_64-pkg-config --host=i686-w64-mingw32
-make -f Makefile.mingw
+./configure --host=x86_64-w64-mingw32
+make
 r1=$?
 mkdir -p ../../../../../target/classes/win64/include
 mkdir -p ../../../../../target/classes/win64/lib
 cp Dist/FreeImage.h ../../../../../target/classes/win64/include
 cp Dist/libfreeimage.a ../../../../../target/classes/win64/lib
 cd ..
-
-rm -R workdir
+rm -r workdir
 
 exit ${r1}
